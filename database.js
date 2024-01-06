3eaa44742f547fd8b601c051697942a3ca746298
Hala Aj <hala.alajrad@gmail.com>
2024-01-06 20:46:03 +0300

    changed the db password

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host     : 'localhost',
 	user     : 'root',
-	password : 'pass1',
+	password : '',
 	database : 'santral_lfdb', 
   waitForConnections: true
 });
